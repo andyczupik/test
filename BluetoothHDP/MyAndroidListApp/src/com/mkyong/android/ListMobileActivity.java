483148f41c7b730c3ac6f7107c785955ab24d56c
563015 <andy@czupik.com>
2012-05-14 15:01:45 -0400

    test

---
+++ BluetoothHDP/MyAndroidListApp/src/com/mkyong/android/ListMobileActivity.java
@@ -0,0 +1,38 @@
+package com.mkyong.android;
+
+import com.mkyong.android.adaptor.MobileArrayAdapter;
+
+import android.app.ListActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+import android.view.View;
+
+public class ListMobileActivity extends ListActivity {
+
+	static final String[] MOBILE_OS = new String[] { "Android", "iOS",
+			"WindowsMobile", "Blackberry"};
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		//setListAdapter(new ArrayAdapter<String>(this, R.layout.list_mobile,
+		//		R.id.label, MOBILE_OS));
+		
+		setListAdapter(new MobileArrayAdapter(this, MOBILE_OS));
+		
+
+	}
+
+	@Override
+	protected void onListItemClick(ListView l, View v, int position, long id) {
+
+		//get selected items
+		String selectedValue = (String) getListAdapter().getItem(position);
+		Toast.makeText(this, selectedValue, Toast.LENGTH_SHORT).show();
+
+	}
+
+}
